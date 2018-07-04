a5973b299b0745ef40c5a8005cd556c66f86fb68
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-07-04 10:53:20 -0700

    started initial object and loaded data

--- includes/main.js
+++ includes/main.js
@@ -1,5 +1,10 @@
-
+var game = null;
+$(document).ready( initialize );
  
 function initialize(){
-
+	var options = {
+		statsArea : $('.statsArea'),
+		cardArea : $(".cardArea")
+	}
+	game = new MemoryMatchGame( options );
 }
